6ec6341942d1b2cf0087b89fd9abf9cdd5ce8ff8
suaneeong <ong.suanee@gmail.com>
2024-02-03 11:29:04 +0800

    Updated script.js

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-function search(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#search-form-input");
-  let city = searchInputElement.value;
-  let apiKey = "f085atc08a41dae309bfobb656f79131";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-
-  axios.get(apiUrl).then(refreshWeather);
-}
-
-function refreshWeather(response) {
+function getWeather(response) {
   let tempElement = document.querySelector(".weather-app-temp");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#weather-app-city");
+
   cityElement.innerHTML = response.data.city;
   tempElement.innerHTML = temperature;
+}
+
+function searchCity(city) {
+  let apiKey = "f085atc08a41dae309bfobb656f79131";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(getWeather);
+}
+
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
