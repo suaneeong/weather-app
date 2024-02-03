cdf5a48c22f8d501f6105da58fbef235848b0725
suaneeong <ong.suanee@gmail.com>
2024-02-03 15:36:31 +0800

    Updated script.js
    Fixed problems

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function refreshWeather(response) {
   let tempElement = document.querySelector(".weather-app-temp");
   let temperature = Math.round(response.data.temperature.current);
-  let cityElement = document.querySelector("#weather-app-city");
+  let cityElement = document.querySelector(".weather-app-city");
 
   cityElement.innerHTML = response.data.city;
   tempElement.innerHTML = temperature;
@@ -15,9 +15,9 @@ function searchCity(city) {
 
 function handleSearchSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector("#search-form-input");
+  let searchInput = document.querySelector(".search-form-input");
   searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
+let searchFormElement = document.querySelector(".search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
