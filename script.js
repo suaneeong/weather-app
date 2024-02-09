5bc585a4b7e87f0cb289dda6ee37c1eef110cf9b
suaneeong <ong.suanee@gmail.com>
2024-02-09 09:47:09 +0800

    Updated js

--- script.js
+++ script.js
@@ -89,4 +89,3 @@ let searchFormElement = document.querySelector(".search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Kuala Lumpur");
-displayForecast();
